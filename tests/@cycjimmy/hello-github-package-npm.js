c55f4eb0e1ef8326b35cff9063b16129bd5de560
cycjimmy <cycjimmy@gmail.com>
2019-10-30 10:55:02 +0800

    update

--- tests/@cycjimmy/hello-github-package-npm.js
+++ tests/@cycjimmy/hello-github-package-npm.js
@@ -2,6 +2,6 @@ const helloGithubPackageNpm = require('@cycjimmy/hello-github-package-npm');
 
 module.exports = () => {
   console.log('\n---- @cycjimmy/hello-github-package-npm ----\n');
-  helloGithubPackageNpm.helloWorld();
+  console.log(helloGithubPackageNpm.helloWorld());
   console.log('\n--------------------------------------------\n');
 };
